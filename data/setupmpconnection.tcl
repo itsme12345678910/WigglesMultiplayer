978ea0d4d73942d47b1dc1c0a42487608bacf3b7
itsme123 <jonas.alber11@yahoo.de>
2024-06-13 17:00:50 +0200

    TCL internen TCP Server durch externen Java Server ersetzt (Multithreading macht es notwendig)

--- data/setupmpconnection.tcl
+++ data/setupmpconnection.tcl
@@ -6,21 +6,20 @@ set server_port 5592
 #Server auf $server_port erstellen
 #set server_thread [thread create]
 #thread eval $server_thread { 
-socket -server incoming $server_port 
+# socket -server incoming $server_port 
 
 #Custom Tcl Command: Update alle 200ms -> neue Commands vom Server Socket
-updateLEG
+#updateLEG
 
 while {[catch {call ./data/connect.tcl} err]} {}
 
-proc incoming {sock addr port} {
-		fileevent $sock readable [list Echo $sock]
-	}
+# proc incoming {sock addr port} {
+		# fileevent $sock readable [list Echo $sock]
+	# }
 	
-proc Echo {sock} {       
-		if {[eof $sock] || [catch {gets $sock line}]} {
-		} else {
-		print $line
-		set rv [eval $line]
-		}
-	}
+# proc Echo {sock} {       
+		# if {[eof $sock] || [catch {gets $sock line}]} {
+		# } else {
+		# eval $line
+		# }
+	# }
---
+++ runServer.bat
@@ -0,0 +1,1 @@
+java -jar WigglesServer.jar 5592
